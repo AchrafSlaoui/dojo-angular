2fbb3e872f3911cfe3917132f421b2c39487dcfe
AchrafSlaoui <57200096+AchrafSlaoui@users.noreply.github.com>
2026-05-25 18:05:31 +0200

    corrige exercice 3

--- src/app/features/accounts/components/account-card/account-card.component.ts
+++ src/app/features/accounts/components/account-card/account-card.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Account } from '@accounts/models/account';
 import { FormatValuePipe } from '@shared/pipes/format-value.pipe';
@@ -14,9 +14,9 @@ import { FormatValuePipe } from '@shared/pipes/format-value.pipe';
 export class AccountCardComponent {
   account = input.required<Account>();
   clientId = input.required<string>();
-  @Input() showStatus = true;
+  showStatus = input(true);
 
-  readonly visibleStatusLabel = computed(() => this.showStatus ? this.statusLabel() : null);
+  readonly visibleStatusLabel = computed(() => this.showStatus() ? this.statusLabel() : null);
 
   readonly statusLabel = computed(() => {
     switch (this.account().status) {
